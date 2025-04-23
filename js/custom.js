a79b965b124196d186d6ef52a78f297cdf36b5d9
dinesh kumar <dineshdmr@gmail.com>
2025-04-23 22:16:52 +0530

    check

--- js/custom.js
+++ js/custom.js
@@ -18,12 +18,59 @@ $(document).ready(function () {
     speed: 500,
     cssEase: "linear",
   });
+
+  let currentStep = 0;
+  const steps = $(".form-step");
+
+  function showStep(index) {
+    steps.eq(currentStep).fadeOut(300, function () {
+      steps.removeClass("active");
+      currentStep = index;
+      steps.eq(currentStep).fadeIn(300).addClass("active");
+    });
+  }
+
+  function validateStep(index) {
+    let isValid = true;
+    const inputs = steps.eq(index).find("input");
+    inputs.each(function () {
+      if (!this.checkValidity()) {
+        $(this).addClass("invalid");
+        isValid = false;
+      } else {
+        $(this).removeClass("invalid");
+      }
+    });
+    return isValid;
+  }
+
+  $(".next-btn").click(function () {
+    if (validateStep(currentStep) && currentStep < steps.length - 1) {
+      showStep(currentStep + 1);
+    }
+  });
+
+  $(".prev-btn").click(function () {
+    if (currentStep > 0) {
+      showStep(currentStep - 1);
+    }
+  });
+
+  // $("#multiStepForm").on("submit", function (e) {
+  //   if (!validateStep(currentStep)) {
+  //     e.preventDefault();
+  //   } else {
+  //     // alert("Form submitted!");
+  //   }
+  // });
+
+  steps.hide().eq(currentStep).show().addClass("active"); // initialize
 });
 
-$(window).scroll(function(){
+$(window).scroll(function () {
   if ($(this).scrollTop() > 36) {
-      $('.header-bottom').addClass('fixed');
+    $(".header-bottom").addClass("fixed");
   } else {
-      $('.header-bottom').removeClass('fixed');
+    $(".header-bottom").removeClass("fixed");
   }
 });
